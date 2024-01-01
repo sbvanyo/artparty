a662849ba64392a4817cf0e216a5a1eea40e1372
Stacey Vanyo <sbvanyo@gmail.com>
2024-01-01 16:52:54 -0600

    fixtures loaded + models tweaked

--- artpartyapi/models/artwork_artist.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from .artist import Artist
-from .artwork import Artwork
-class ArtworkArtist(models.Model):
-
-    artwork = models.ForeignKey(Artwork, on_delete=models.CASCADE)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
